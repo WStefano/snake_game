a3689286a48842bbfc6ea3e1f6bb3bd59a872db4
Wesley Amadeu Stefano <amadeuzinho34@hotmail.com>
2021-03-28 14:28:11 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# snake_game
+ Esse é o famoso "Snake game" (vulgo jogo da cobrinha) feito em c# no visual studio
---
+++ snake_game/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_game
+{
+    class Circle
+    {
+        public int X
+        {
+            get;
+            set; 
+        }
+
+        public int Y
+        {
+            get;
+            set;
+        }
+
+        public Circle()
+        {
+            X = 0;
+            Y = 0;
+        }
+    }
+}
